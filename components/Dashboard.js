7e4b1d8e45e530e19b6e7b35be85fc3b8b0905da
mishaelmatende <mishaelmatende@gmail.com>
2025-03-11 16:44:38 +0300

    dashboard layout

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,4 +1,8 @@
+import { getModifiedCookieValues } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { Fugaz_One } from "next/font/google";
 import React from "react";
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function Dashboard() {
   const statuses = {
@@ -6,17 +10,57 @@ export default function Dashboard() {
     time_remaining: "13:14:26",
     date: new Date().toDateString(),
   };
+
+  const moods = {
+    "$%#@&": "😭",
+    sad: "😥",
+    Existing: "😶",
+    Good: "☺️",
+    Elated: "😍",
+  };
+
   return (
-    <div className="flex flex-col flex-1">
-      <div className="grid grid-cols-1 sm:grid-cols-3">
+    <div className="flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16">
+      <div className="grid grid-cols-3 bg-indigo-50 text-indigo-500 rounded-lg p-4 gap-4">
         {Object.keys(statuses).map((status, statusIndex) => {
           return (
-            <div key={statusIndex}>
-              <p className="font-medium uppercase">
+            <div key={statusIndex} className="flex flex-col gap-1 sm:gap-2">
+              <p className="font-medium uppercase text-xs sm:text-sm truncate">
                 {status.replaceAll("_", " ")}
               </p>
-              <p>{statuses[status]}</p>
+              <p className={"text-base sm:text-lg truncate " + fugaz.className}>
+                {statuses[status]}
+              </p>
             </div>
+          );
+        })}
+      </div>
+      <h4
+        className={
+          "text-5xl sm:text-6xl md:text-7xl text-center " + fugaz.className
+        }
+      >
+        How do you <span className="text-gradient">feel</span> today?
+      </h4>
+      <div className="flex items-stretch flex-wrap gap-4">
+        {Object.keys(moods).map((mood, moodIndex) => {
+          return (
+            <button
+              className={
+                "p-4 px-5 rounded-2xl purple-shadow duration-200 bg-indigo-50 hover:bg-indigo-100 text-center flex flex-col gap-2 items-center flex-1 "
+              }
+              key={moodIndex}
+            >
+              <p className="text-4xl sm:text-5xl md:text-6xl">{moods[mood]}</p>
+              <p
+                className={
+                  "text-indigo-500 text-xs sm:text-sm md:text-base " +
+                  fugaz.className
+                }
+              >
+                {mood}
+              </p>
+            </button>
           );
         })}
       </div>
