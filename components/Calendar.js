3cd5a4862773672fb8fbc46e4e83084355f7ad51
mishaelmatende <mishaelmatende@gmail.com>
2025-04-11 15:01:28 +0300

    Final commit

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,6 +1,9 @@
 "use client";
 import { baseRating, demoData, gradients } from "@/utils";
+import { Fugaz_One } from "next/font/google";
 import React, { useState } from "react";
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 const months = {
   January: "Jan",
@@ -16,6 +19,7 @@ const months = {
   November: "Nov",
   December: "Dec",
 };
+const monthsArr = Object.keys(months);
 const now = new Date();
 const dayList = [
   "Sunday",
@@ -37,12 +41,23 @@ export default function Calendar(props) {
   );
   const [selectedYear, setSelectedYear] = useState(now.getFullYear());
 
-  const numericMonth = Object.keys(months).indexOf(selectedMonth);
+  const numericMonth = monthsArr.indexOf(selectedMonth);
   const data = completeData?.[selectedYear]?.[numericMonth] || {};
   function handleIncrementMonth(val) {
     // value +1 -1
     // if we hit the bounds of the months, then we can adjust
     // the year that is displayed
+    if (numericMonth + val < 0) {
+      // set month value = 11 and decrement the year
+      setSelectedYear((curr) => curr - 1);
+      setSelectedMonth(monthsArr[monthsArr.length - 1]);
+    } else if (numericMonth + val > 11) {
+      // set month val = 0 and increment the year
+      setSelectedYear((curr) => curr + 1);
+      setSelectedMonth(monthsArr[0]);
+    } else {
+      setSelectedMonth(monthsArr[numericMonth + val]);
+    }
   }
 
   console.log("SELECTED MONTH: ", selectedMonth);
@@ -65,49 +80,77 @@ export default function Calendar(props) {
   const numRows = Math.floor(daysToDisplay / 7) + (daysToDisplay % 7 ? 1 : 0);
 
   return (
-    <div className="flex flex-col overflow-hidden gap-1 py-4 sm:py-6 md:py-10">
-      {[...Array(numRows).keys()].map((row, rowIndex) => {
-        return (
-          <div key={rowIndex} className="grid grid-cols-7 gap-1">
-            {dayList.map((dayOfWeek, dayOfWeekIndex) => {
-              let dayIndex =
-                rowIndex * 7 + dayOfWeekIndex - (firstDayOfMonth - 1);
-              let dayDisplay =
-                dayIndex > daysInMonth
-                  ? false
-                  : row === 0 && dayOfWeekIndex < firstDayOfMonth
-                  ? false
-                  : true;
+    <div className="flex flex-col gap-2">
+      <div className="grid grid-cols-5 gap-4">
+        <button
+          onClick={() => {
+            handleIncrementMonth(-1);
+          }}
+          className="mr-auto text-indigo-400 text-lg sm:text-xl duration-200 hover:opacity-60"
+        >
+          <i className="fa-solid fa-circle-chevron-left"></i>
+        </button>
+        <p
+          className={
+            "text-center capitalized col-span-3 whitespace-nowrap text-gradient " +
+            fugaz.className
+          }
+        >
+          {selectedMonth}, {selectedYear}
+        </p>
+        <button
+          onClick={() => {
+            handleIncrementMonth(+1);
+          }}
+          className="ml-auto text-indigo-400 text-lg sm:text-xl duration-200 hover:opacity-60"
+        >
+          <i className="fa-solid fa-circle-chevron-right"></i>
+        </button>
+      </div>
+      <div className="flex flex-col overflow-hidden gap-1 py-4 sm:py-6 md:py-10">
+        {[...Array(numRows).keys()].map((row, rowIndex) => {
+          return (
+            <div key={rowIndex} className="grid grid-cols-7 gap-1">
+              {dayList.map((dayOfWeek, dayOfWeekIndex) => {
+                let dayIndex =
+                  rowIndex * 7 + dayOfWeekIndex - (firstDayOfMonth - 1);
+                let dayDisplay =
+                  dayIndex > daysInMonth
+                    ? false
+                    : row === 0 && dayOfWeekIndex < firstDayOfMonth
+                    ? false
+                    : true;
 
-              let isToday = dayIndex === now.getDate();
+                let isToday = dayIndex === now.getDate();
 
-              if (!dayDisplay) {
-                return <div className="bg-white" key={dayOfWeekIndex} />;
-              }
+                if (!dayDisplay) {
+                  return <div className="bg-white" key={dayOfWeekIndex} />;
+                }
 
-              let color = demo
-                ? gradients.indigo[baseRating[dayIndex]]
-                : dayIndex in data
-                ? gradients.indigo[data[dayIndex]]
-                : "white";
+                let color = demo
+                  ? gradients.indigo[baseRating[dayIndex]]
+                  : dayIndex in data
+                  ? gradients.indigo[data[dayIndex]]
+                  : "white";
 
-              return (
-                <div
-                  style={{ background: color }}
-                  className={
-                    "text-xs sm:text-sm border border-solid p-2 flex items-center gap-2 jusitfy-between rounded-lg " +
-                    (isToday ? " border-indigo-400" : " border-indigo-100 ") +
-                    (color === "white" ? " text-indigo-400 " : " text-white")
-                  }
-                  key={dayOfWeekIndex}
-                >
-                  <p>{dayIndex}</p>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                return (
+                  <div
+                    style={{ background: color }}
+                    className={
+                      "text-xs sm:text-sm border border-solid p-2 flex items-center gap-2 jusitfy-between rounded-lg " +
+                      (isToday ? " border-indigo-400" : " border-indigo-100 ") +
+                      (color === "white" ? " text-indigo-400 " : " text-white")
+                    }
+                    key={dayOfWeekIndex}
+                  >
+                    <p>{dayIndex}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Calendar from "./Calendar";
 import { useAuth } from "@/context/AuthContext";
 import { doc, setDoc } from "firebase/firestore";
+import { db } from "@/firebase";
 import Loading from "./Loading";
 import Login from "./Login";
 
@@ -12,14 +13,36 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 export default function Dashboard() {
   const { currentUser, userDataObj, setUserDataObj, loading } = useAuth();
   const [data, setData] = useState({});
+  const now = new Date();
 
-  function countValues() {}
+  function countValues() {
+    let total_number_of_days = 0;
+    let sum_moods = 0;
+    for (let year in data) {
+      for (let month in data[year]) {
+        for (let day in data[year][month]) {
+          let days_mood = data[year][month][day];
+          total_number_of_days++;
+          sum_moods += days_mood;
+        }
+      }
+    }
+    return {
+      num_days: total_number_of_days,
+      average_mood: sum_moods / total_number_of_days,
+    };
+  }
+
+  const statuses = {
+    ...countValues(),
+    time_remaining: `${23 - now.getHours()}H ${60 - now.getMinutes()}M`,
+  };
 
   async function handleSetMood(mood) {
-    const now = new Date();
     const day = now.getDate();
     const month = now.getMonth();
     const year = now.getFullYear();
+
     try {
       const newData = { ...userDataObj };
       if (!newData?.[year]) {
@@ -28,13 +51,13 @@ export default function Dashboard() {
       if (!newData?.[year]?.[month]) {
         newData[year][month] = {};
       }
-      newData[year][month][day] = mood;
 
-      //Update Current state
+      newData[year][month][day] = mood;
+      // update the current state
       setData(newData);
-      //Update the global state
+      // update the global state
       setUserDataObj(newData);
-      //Update firebase
+      // update firebase
       const docRef = doc(db, "users", currentUser.uid);
       const res = await setDoc(
         docRef,
@@ -51,12 +74,6 @@ export default function Dashboard() {
       console.log("Failed to set data: ", err.message);
     }
   }
-
-  const statuses = {
-    num_days: 14,
-    time_remaining: "13:14:26",
-    date: new Date().toDateString(),
-  };
 
   const moods = {
     "$%#@&": "😭",
@@ -87,11 +104,11 @@ export default function Dashboard() {
         {Object.keys(statuses).map((status, statusIndex) => {
           return (
             <div key={statusIndex} className="flex flex-col gap-1 sm:gap-2">
-              <p className="font-medium uppercase text-xs sm:text-sm truncate">
+              <p className="font-medium capitalize text-xs sm:text-sm truncate">
                 {status.replaceAll("_", " ")}
               </p>
               <p className={"text-base sm:text-lg truncate " + fugaz.className}>
-                {statuses[status]}
+                {statuses[status]} {status === "num_days" ? "🔥" : ""}
               </p>
             </div>
           );
