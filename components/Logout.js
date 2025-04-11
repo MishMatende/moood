4cff32627f05d510513dd63e64b70a448a8cbc5f
mishaelmatende <mishaelmatende@gmail.com>
2025-04-11 15:02:47 +0300

    grammar

--- components/Logout.js
+++ components/Logout.js
@@ -16,7 +16,7 @@ export default function Logout() {
   if (pathname === "/") {
     return (
       <Link href="/dashboard">
-        <Button text="Go to dahsboard" />
+        <Button text="Go to dashboard" />
       </Link>
     );
   }
